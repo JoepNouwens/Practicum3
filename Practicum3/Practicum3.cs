c8932737a62cc6235045a6f2e6a2a0b21f744b32
JoepNouwens <45453844+JoepNouwens@users.noreply.github.com>
2018-12-21 12:20:40 +0100

    Update Practicum3.cs

--- Practicum3/Practicum3.cs
+++ Practicum3/Practicum3.cs
@@ -1,95 +1,211 @@
-﻿using System;
+using System;
+using System.Windows.Forms;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Windows.Forms;
+using System.Windows.Input;
 
-namespace Practicum3
+namespace Reversi
 {
-    class Scherm : Form
+    class Reversischerm : Form
     {
         //Members
-        public PictureBox plaatje;
-        public Bitmap spelBord;
-        public Button nieuwSpel;
-        public Button help;
+        private Button help, nieuwspel;
+        private Panel panel;
+        private Label roodstenen, blauwstenen, aanzet;
+        private int grootte = 650, xpanelgrootte, ypanelgrootte, diameter = 50, muisX, muisY, spelgrootteX, spelgrootteY;
+        int wit = 0, blauw = 1, rood = 2;
+        int aantalBlauw = 2, aantalRood = 2;
+        int[,] veld;
+        bool geklikt;
+        int beurt;
 
-        public Scherm()
+        public Reversischerm()
         {
-            //Scherm
+            //Form
             this.Text = "Reversi";
-            this.ClientSize = new System.Drawing.Size(500, 600);
-            this.BackColor = Color.FromArgb(204, 204, 255);
+            this.ClientSize = new Size(grootte, grootte);
+            this.BackColor = Color.LightGray;
 
-            //Knoppen
+            //Nieuwspel knop
+            nieuwspel = new Button();
+            nieuwspel.Text = "Nieuw spel";
+            nieuwspel.Location = new Point(200, 75);
+            nieuwspel.Size = new Size(75, 50);
+
+            //Help knop
             help = new Button();
+            help.Text = "Help";
+            help.Location = new Point(325, 75);
+            help.Size = new Size(75, 50);
+
+            //Aantal rode stenen
+            roodstenen = new Label();
+            roodstenen.Text = aantalRood.ToString("") + " stenen";
+            roodstenen.Location = new Point(150, 150);
+            roodstenen.ForeColor = Color.Red;
+
+            //Aantal blauwe stenen
+            blauwstenen = new Label();
+            blauwstenen.Text = aantalBlauw.ToString("") + " stenen";
+            blauwstenen.Location = new Point(150, 200);
+            blauwstenen.ForeColor = Color.Blue;
+            
+            aanzet = new Label();
+            String beurtS;
+            beurt = blauw;
+            if (beurt == blauw)
             {
-                help.Text = "Help";
-                help.Location = new Point(ClientSize.Width / 2 + 100, 40);
-                help.Size = new Size(80, 40);
-            };
+                beurtS = "Blauw";
+            }
+            if (beurt == rood)
+            {
+                beurtS = "Rood";
+            }
+            else 
+            {
+                beurtS = "Iets";
+            }
+            aanzet.Text = beurtS + " is aan zet";
+            aanzet.Location = new Point(150, 250);
+
+
+            //Panel (grootte verandert als )
+            spelgrootteX = 6; 
+            spelgrootteY = 5;
+            xpanelgrootte = spelgrootteX * 50;
+            ypanelgrootte = spelgrootteY * 50;
+            panel = new Panel();
+            panel.Location = new Point(grootte * 3 / 13, grootte / 2 - 25);
+            panel.Size = new Size(xpanelgrootte, ypanelgrootte);
+            panel.BackColor = Color.White;
+            panel.BorderStyle = BorderStyle.Fixed3D;
+
+            this.Controls.Add(nieuwspel);
             this.Controls.Add(help);
+            this.Controls.Add(panel);
+            this.Controls.Add(roodstenen);
+            this.Controls.Add(blauwstenen);
+            this.Controls.Add(aanzet);
+            this.Paint += CirkeltjesNaastAantal;
+            panel.Paint += Startpositie;
+            panel.MouseClick += PlaatsSteen;
+            //help.Click += Help;
+            nieuwspel.Click += NieuwBord;
 
-            nieuwSpel = new Button();
+            veld = new int[6, 6];
+        }
+
+        void CirkeltjesNaastAantal(object o, PaintEventArgs pea)
+        {
+            pea.Graphics.FillEllipse(Brushes.Red, 105, 142, 30, 30);
+            pea.Graphics.FillEllipse(Brushes.Blue, 105, 192, 30, 30);
+        }
+
+        void Startpositie(object o, PaintEventArgs pea)
+        {
+
+            Pen zwart = new Pen(Color.Black)
             {
-                nieuwSpel.Text = "Nieuw Spel";
-                nieuwSpel.Location = new Point(ClientSize.Width / 2 - 80 - 100, 40);
-                nieuwSpel.Size = new Size(80, 40);
+                Width = 2,
             };
-            this.Controls.Add(nieuwSpel);
 
-            //PictureBox
-            plaatje = new PictureBox();
+            veld = new int[spelgrootteX, spelgrootteY];
+
+            for (int x = 0; x < veld.GetLength(0); x++)
+                for (int y = 0; y < veld.GetLength(1); y++)
+                    pea.Graphics.DrawRectangle(zwart, x * 50, y * 50, 50, 50);
+
+            veld[spelgrootteX/2-1, spelgrootteX/2-1] = blauw;
+            veld[spelgrootteX/2, spelgrootteX/2-1] = rood;
+            veld[spelgrootteX/2, spelgrootteX/2] = blauw;
+            veld[spelgrootteX/2-1, spelgrootteX/2] = rood;
+
+            for (int x = 0; x < spelgrootteX; x++)
             {
-                plaatje.BorderStyle = BorderStyle.Fixed3D;
-                plaatje.Location = new Point(ClientSize.Width / 2 - 150, ClientSize.Height / 3);
-                plaatje.Size = new Size(300, 300);
-                plaatje.Image = spelBord;
-            };
-            this.Controls.Add(this.plaatje);
-            plaatje.Paint +=tekenSpelBord;
+                for (int y = 0; y < spelgrootteY; y++)
+                {
+                    if (veld[x, y] != blauw && veld[x, y] != rood)
+                        veld[x, y] = wit;
 
-           void tekenSpelBord(object obj, PaintEventArgs pea)
+                    if (veld[x, y] == blauw)
+                        pea.Graphics.FillEllipse(Brushes.Blue, x * 50, y * 50, diameter, diameter);
+
+                    if (veld[x, y] == rood)
+                        pea.Graphics.FillEllipse(Brushes.Red, x * 50, y * 50, diameter, diameter);
+                }
+            }
+
+            //Mooiere cirkels
+            pea.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        }
+
+        /* void TekenSteen(object sender, PaintEventArgs pea)
+        {
+            if (beurt == blauw)
             {
-                Graphics g = pea.Graphics;
-                spelBord = new Bitmap(plaatje.Size.Width, plaatje.Size.Height);
-                int lijnen = 7;
-                int afstand = 50;
-                Pen zwart = new Pen(Color.Black)
-                {
-                    Width = 2
-                };
-                int x = 0;
-                int y = 0;
-                
+                pea.Graphics.FillEllipse(Brushes.Blue, arrayX, arrayY, diameter, diameter);
+            }
+            else
+            {
+                pea.Graphics.FillEllipse(Brushes.Red, arrayX, arrayY, diameter, diameter);
+            }
 
-                //Horizontale lijnen
-                for (y = 0; y < lijnen; ++y)
-                {
-                    g.DrawLine(zwart, 0, y * afstand, lijnen * afstand, y * afstand);
-                }
+        } */
 
-                //Verticale lijnen
-                for (x = 0; x < lijnen; ++x)
-                {
-                    g.DrawLine(zwart, x * afstand, 0, x * afstand, lijnen * afstand);
-                }
-                this.BackgroundImage = spelBord;
+        void PlaatsSteen(object sender, MouseEventArgs mea)
+        {
+            muisX = (int)mea.X / 50;
+            muisY = (int)mea.Y / 50;
+
+            if (veld[muisX, muisY] == wit && beurt == blauw)
+            {
+                geklikt = true;
+                aantalBlauw++;
+                blauwstenen.Invalidate();
+                panel.Invalidate();
+                Beurt();
+            }
+            else if (veld[muisX, muisY] == wit && beurt == rood)
+            {
+                geklikt = true;
+                aantalRood++;
+                roodstenen.Invalidate();
+                panel.Invalidate();
+                Beurt();
             }
         }
 
-        static void Main()
+        void Beurt()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Scherm());
+            if (beurt == blauw && geklikt == true)
+            {
+                beurt = rood;
+            }
+
+            if (beurt == rood && geklikt == true)
+            {
+                beurt = blauw;
+            }
         }
 
+        void Help(object sender, EventArgs e)
+        {
+
+        }
+
+        void NieuwBord(object sender, EventArgs e)
+        {
+            //Startpositie();
+        }
     }
 
-     public class BitmapControl : UserControl
+    class Program
+    {
+        public static void Main()
         {
-        
+            Application.Run(new Reversischerm());
+        }
     }
 }
